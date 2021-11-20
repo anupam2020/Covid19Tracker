c1fb36f124d0ce48ef23e3e33ab5f630233283ec
Anupam Basak <anupam00basak@gmail.com>
2021-11-21 01:45:48 +0530

    API calling in My Country done!

--- app/src/main/java/com/sbdev/covid19tracker/MyCountryFragment.java
+++ app/src/main/java/com/sbdev/covid19tracker/MyCountryFragment.java
@@ -1,5 +1,7 @@
 package com.sbdev.covid19tracker;
 
+import android.app.ProgressDialog;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -12,11 +14,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import org.eazegraph.lib.charts.BarChart;
+import org.eazegraph.lib.charts.PieChart;
+import org.eazegraph.lib.charts.ValueLineChart;
+import org.eazegraph.lib.models.BarModel;
+import org.eazegraph.lib.models.PieModel;
+import org.eazegraph.lib.models.ValueLinePoint;
+import org.eazegraph.lib.models.ValueLineSeries;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -27,11 +38,104 @@ import okhttp3.Response;
 
 public class MyCountryFragment extends Fragment {
 
+    private TextView affected,death,recovered,active,newAffected,newDeath,newRecovered,critical;
+
+    private int affCount,deathCount,recCount,actCount,newAffCount,newDeathCount,newRecCount,critCount;
+
+    private ProgressDialog progressDialog;
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        affected=view.findViewById(R.id.textAffectedCountCountry);
+        death=view.findViewById(R.id.textDeathCountCountry);
+        recovered=view.findViewById(R.id.textRecoveredCountCountry);
+        active=view.findViewById(R.id.textActiveCountCountry);
+        newAffected=view.findViewById(R.id.textNewCasesCountCountry);
+        newDeath=view.findViewById(R.id.textNewDeathsCountCountry);
+        newRecovered=view.findViewById(R.id.textNewRecoveredCountCountry);
+        critical=view.findViewById(R.id.textCriticalCountCountry);
+
+        progressDialog=new ProgressDialog(getActivity());
+
+        progressDialog.show();
+        progressDialog.setContentView(R.layout.loading_bg);
+        progressDialog.setCancelable(false);
+        progressDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/npm-covid-data/country-report-iso-based/India/ind")
+                .get()
+                .addHeader("x-rapidapi-host", "vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com")
+                .addHeader("x-rapidapi-key", "e8f6c57650msh666fef2e3a110b5p13b950jsn4359d608e124")
+                .build();
+
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+                Log.e("OnFailure",e.getMessage());
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+                if(response.isSuccessful())
+                {
+
+                    String res=response.body().string();
+
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            try {
+
+                                JSONArray jsonArray=new JSONArray(res);
+                                JSONObject jsonObject=jsonArray.getJSONObject(0);
+
+                                affCount=jsonObject.getInt("TotalCases");
+                                deathCount=jsonObject.getInt("TotalDeaths");
+                                recCount=jsonObject.getInt("TotalRecovered");
+                                actCount=jsonObject.getInt("ActiveCases");
+                                newAffCount=jsonObject.getInt("NewCases");
+                                newDeathCount=jsonObject.getInt("NewDeaths");
+                                newRecCount=jsonObject.getInt("NewRecovered");
+                                critCount=jsonObject.getInt("Serious_Critical");
+
+
+                                affected.setText(getFormatedAmount(affCount));
+                                death.setText(getFormatedAmount(deathCount));
+                                recovered.setText(getFormatedAmount(recCount));
+                                active.setText(getFormatedAmount(actCount));
+                                newAffected.setText(getFormatedAmount(newAffCount));
+                                newDeath.setText(getFormatedAmount(newDeathCount));
+                                newRecovered.setText(getFormatedAmount(newRecCount));
+                                critical.setText(getFormatedAmount(critCount));
+
+
+                                progressDialog.dismiss();
+
+                            } catch (JSONException e) {
+                                Log.e("Catch",e.getMessage());
+                            }
+
+                        }
+                    });
+
+                }
+
+            }
+        });
+
+    }
+
+    private static String getFormatedAmount(int amount){
+        return NumberFormat.getNumberInstance(Locale.US).format(amount);
     }
 
     @Override
